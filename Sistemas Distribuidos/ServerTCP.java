c6121b6d434fe626ac2eff3bde5cb0313b7839a9
João Vitor <31751277+joaovitorx6@users.noreply.github.com>
2020-03-11 17:30:51 -0300

    Create ServerTCP.java

---
+++ Sistemas Distribuidos/ServerTCP.java
@@ -0,0 +1,31 @@
+package tcp;
+import java.net.*;
+import java.net.ServerSocket;
+import java.io.IOException;
+import java.net.Socket;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author 20181014040030
+ */
+public class ServerTCP {
+
+    public ServerTCP(){
+        try {
+            ServerSocket sst = new ServerSocket(3030);
+            System.out.println("Aguardando novas conexões.");
+            Socket s = sst.accept();
+            OutputStream os = s.getOutputStream();
+            byte [] mensagem = "Coronga virus".getBytes();
+            os.write(mensagem);
+            os.close();
+        } catch (IOException ioex) {
+               ioex.printStackTrace();
+        }
+        
+    }
+     public static void main(String[] args) {
+            ServerTCP st = new ServerTCP();
+        }
+}
